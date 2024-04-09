37e6a3dec199a28aba7302d44cf26cf4f90796e6
glblg <gianlucalabruna@gmail.com>
2024-04-09 21:15:48 +0200

    Assignment 1
    Assignment 1 Gianluca La Bruna

---
+++ preperandkurs/assignments/Assignment 1 Gianluca La Bruna /Scripts/script.js
@@ -0,0 +1,25 @@
+//optional
+function log() {
+    console.log('divide')
+}
+
+let foo= 0
+
+let bar= 0
+
+console.log(foo / bar)
+
+let result = foo / bar
+
+
+//Jag försökte förstå hur man löser det valfria uppdraget i punkt 3, men jag förstod inte. Jag antar att för att visa det som en korrekt varning bör du länka det till HTML? Eller göra en if-else statement? Om vi kunde ta det i klassen skulle det vara fantastiskt. Tack!
+if (isNaN(result)) {
+    alert("You can't divide us!");
+
+}
+
+
+
+
+    
+
